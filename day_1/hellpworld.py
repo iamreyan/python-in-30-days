f01f574ad08d026caffa6f2f331afe23267049f8
Reyan A <iamreyan@proton.me>
2023-12-22 21:24:10 +0500

    Day 1 , level 2

---
+++ day_1/hellpworld.py
@@ -0,0 +1,23 @@
+print(4-3)
+print(4+3)
+print(4*3)
+print(4/3)
+print(4//3)
+print(4**3)
+
+
+
+print("Reyan")
+print("My family")
+print("Pakistan")
+print("I am enjoying 30 days of python")
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['ali','reyan']))
+print(type('Reyan'))
+print(type('My family'))
+print(type('Pakistan'))

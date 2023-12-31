7fe043fdceb9585434e4d782b7a0742b78491172
Reyan A <iamreyan@proton.me>
2023-12-31 18:43:28 +0500

    day_2,( till Exercises, level 2, Q.5, part 2.)

---
+++ day_2/Variables.py
@@ -0,0 +1,34 @@
+first_name = 'Reyan'
+last_name = 'Asghar'
+full_name = 'Reyan Asghar'
+country = 'Pakistan'
+city = 'Rahim Yar Khan'
+age = 15
+year = 2023
+is_married = False
+is_true = 'Yes'
+is_light_on = 'Yes'
+first_name, last_name, country, age, is_married = 'Reyan', 'Asghar', 'Rahim Yar Khan', 15, False
+
+# print(type(first_name))
+# print(type(last_name))
+# print(type(full_name))
+# print(type(country))
+# print(type(city))
+# print(type(age))
+# print(type(year))
+# print(type(is_married))
+# print(type(is_true))
+# print(type(is_light_on))
+# print(len(first_name))
+# print(len(last_name))
+
+num_one = 5
+num_two =  4
+
+diff = print(num_two-num_one)
+product = print(num_one*num_two)
+total = print(num_one+num_two)
+division = print(num_one/num_two)
+reminder = print(num_two//num_one)
+exp = print(num_one**num_two)

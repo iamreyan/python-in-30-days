d6a7d69df14ccabd7e037810772f4172d0a1d6e6
Reyan A <iamreyan@proton.me>
2023-12-31 18:48:06 +0500

    done Day 2 ( till Exercises - Day 2 - Level 2 - Question 5, part 2)

--- day_2/Variables.py
+++ day_2/Variables.py
@@ -10,18 +10,18 @@ is_true = 'Yes'
 is_light_on = 'Yes'
 first_name, last_name, country, age, is_married = 'Reyan', 'Asghar', 'Rahim Yar Khan', 15, False
 
-# print(type(first_name))
-# print(type(last_name))
-# print(type(full_name))
-# print(type(country))
-# print(type(city))
-# print(type(age))
-# print(type(year))
-# print(type(is_married))
-# print(type(is_true))
-# print(type(is_light_on))
-# print(len(first_name))
-# print(len(last_name))
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(len(first_name))
+print(len(last_name))
 
 num_one = 5
 num_two =  4
@@ -30,5 +30,11 @@ diff = print(num_two-num_one)
 product = print(num_one*num_two)
 total = print(num_one+num_two)
 division = print(num_one/num_two)
-reminder = print(num_two//num_one)
+floor_division = print(num_two//num_one)
 exp = print(num_one**num_two)
+remainder = print(num_two % num_one)
+
+
+r = 30       # r = radius
+area_of_circle = print(3.142*30**2)
+circum_of_circle = print(2*3.142*30)

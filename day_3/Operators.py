07170ebe471418b5ee969d9fde718ccf3d760d3c
Reyan A <iamreyan@proton.me>
2024-01-04 12:35:13 +0500

    done till Q. 7

---
+++ day_3/Operators.py
@@ -0,0 +1,52 @@
+age = int(15.6)
+height = float(55.5)
+print(complex(5 + 5j))
+
+
+base = int(input("Please enter your base: "))
+height = int(input("Please enter your height: "))
+length = float(0.5)
+area = length*base*height
+print(area)
+
+
+a = int(input("What is side_a?: "))
+b = int(input("What is side_b?: "))
+c = int(input("What is side_c?: "))
+p = a + b + c                      # p = perimeter
+print(p)
+
+
+l = int(input("Please enter the length: "))
+w = int(input("Please enter the width: "))
+area = l*w
+perimeter = 2*(l+w)
+print(area)
+print(perimeter)
+
+
+r = int(input("What is radius?:"))
+pi = float(3.14)
+area = pi * r * r
+circumference = 2 * pi * r
+print(area)
+print(circumference)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

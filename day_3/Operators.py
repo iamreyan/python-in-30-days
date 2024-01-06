20eb2ec8ac4cde41ee4348e7a6c78c1bfaffcb89
Reyan A <iamreyan@proton.me>
2024-01-06 13:01:13 +0500

    day 3 done (Q. 8-11,13 skipped)

--- day_3/Operators.py
+++ day_3/Operators.py
@@ -2,20 +2,17 @@ age = int(15.6)
 height = float(55.5)
 print(complex(5 + 5j))
 
-
 base = int(input("Please enter your base: "))
 height = int(input("Please enter your height: "))
 length = float(0.5)
 area = length*base*height
 print(area)
 
-
 a = int(input("What is side_a?: "))
 b = int(input("What is side_b?: "))
 c = int(input("What is side_c?: "))
 p = a + b + c                      # p = perimeter
 print(p)
-
 
 l = int(input("Please enter the length: "))
 w = int(input("Please enter the width: "))
@@ -24,7 +21,6 @@ perimeter = 2*(l+w)
 print(area)
 print(perimeter)
 
-
 r = int(input("What is radius?:"))
 pi = float(3.14)
 area = pi * r * r
@@ -32,21 +28,32 @@ circumference = 2 * pi * r
 print(area)
 print(circumference)
 
+a = print(len("python"))
+b = print(len("dragon"))                     
 
+c = "I hope this course is not full of jargon."
+print('jargon' in 'I hope this course is not full of jargon.')
 
+print(float(len("python")))
+print(str(len("python")))
 
+print(int(7//3))
 
+print('10' == 10)
 
+print('9.8' == 10)
 
+hours = int(input('Enter hours:'))
+rate_per_hours = int(input('Enter rate per hours:'))
+income = hours*rate_per_hours
+print("Your weekly earning is", income)
 
+years = int(input("Enter number of years you have lived:"))
+seconds_in_a_year = int(365*24*60*60)
+print(seconds_in_a_year*years)
 
-
-
-
-
-
-
-
-
-
-
+print("1 1 1 1 1")
+print('2 1 2 4 8')
+print('3 1 3 9 27')
+print('4 1 4 16 64')
+print("5 1 5 25 125")

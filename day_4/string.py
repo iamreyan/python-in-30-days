147acb1f38f24b8c40c20fb9f3ecdabc43022f5e
Reyan A <iamreyan@proton.me>
2024-01-18 20:50:00 +0500

    day 4 until 13.

---
+++ day_4/string.py
@@ -0,0 +1,127 @@
+a = 'Thirty'
+b = 'Days'
+c = 'Of'
+d = 'Python'
+s = ' '
+full_a_b_c_d = a+s+b+s+c+s+d
+print(full_a_b_c_d)
+
+e = "Coding"
+f = 'For'
+g = 'All'
+full_e_f_g = e+s+f+s+g 
+print(full_e_f_g)
+
+company = "Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+pto = company[1:14]
+print(pto)
+print(company.startswith('Coding'))
+print(company.replace('Coding','Python'))
+sen = 'Python For Everyone'
+print(sen.replace("Everyone",'All'))
+print(company.split())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

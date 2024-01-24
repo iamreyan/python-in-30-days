0ee8ae01d7faa5feac3209c3e082954e5d9aec2a
Reyan A <iamreyan@proton.me>
2024-01-24 20:31:34 +0500

    day 5 till Q.26

--- day_5/lists.py
+++ day_5/lists.py
@@ -9,24 +9,35 @@ print(middle_name)
 print(last_name) 
 mixed_data_types = ["reyan",15.5,'30cm','not_maried','10/A,DHA,Multan']   #name+age+height+marital status+address
 print(mixed_data_types)
-companies = 'facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon' 
+companies = ['facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'] 
 print(companies)
 print(len(companies))
 first_company = 'facebook'
 middle_company = 'Apple'
 last_company = 'Amazon'
 print(first_company, middle_company, last_company)
-companies_2 = 'facebook', 'Google', 'Microsoft', 'linux','Apple', 'IBM', 'Oracle', 'Amazon'
+companies_2 = ['facebook', 'Google', 'Microsoft', 'linux','Apple', 'IBM', 'Oracle', 'Amazon']
 a_companies = 'google'
 print(a_companies.upper())
-
-
-
-
-
-
-
-
+string = '#;'
+print('facebook',string,'Google',string, 'Microsoft',string, 'linux',string,'Apple',string, 'IBM',string, 'Oracle',string, 'Amazon')
+companies_2.sort()
+companies_2.sort(reverse=True)
+print(sorted(companies_2))
+print(sorted(companies_2,reverse=True))
+first_company = 'facebook'
+second_company = 'Google'
+third_company = 'Microsoft'
+first_last_company = 'Amazon'
+second_last_company = 'Oracle'
+third_last_company = 'IBM'
+print(first_company,second_company,third_company,first_last_company,second_last_company,third_last_company)
+companies.clear()
+print((companies))
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+end = front_end+back_end
+print(end)
 
 
 

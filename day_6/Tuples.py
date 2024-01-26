a66f77f10186f2f71fbef4570a750cad110ce7eb
Reyan A <iamreyan@proton.me>
2024-01-26 19:34:07 +0500

    day 6 till level 2 Q.3

---
+++ day_6/Tuples.py
@@ -0,0 +1,114 @@
+empty_tuple = tuple()
+brother = ('ali','ahmed','saim','akbar')
+sister = ('nadia','fiaqa')            # random names of girls
+siblings = brother + sister
+print(len(siblings))
+parents = ('Mr. Asghar Ali',' Ms. Asghar') 
+print(siblings,parents)
+animals = ('dog','cat','rabbit')
+vegetables = ('onion','potato','tomato')
+fruits = ('apple','mango','orange')
+food_stuff_tp = fruits, vegetables, animals
+food_stuff_It = fruits, vegetables, animals
+food_stuff_tp = list(food_stuff_It)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

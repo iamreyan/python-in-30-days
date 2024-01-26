b531b9d39072eb2b1fa6d786c33fe4cedbdf4383
Reyan A <iamreyan@proton.me>
2024-01-26 20:06:06 +0500

    day 6 done

--- day_6/Tuples.py
+++ day_6/Tuples.py
@@ -11,8 +11,10 @@ fruits = ('apple','mango','orange')
 food_stuff_tp = fruits, vegetables, animals
 food_stuff_It = fruits, vegetables, animals
 food_stuff_tp = list(food_stuff_It)
-
-
+del food_stuff_tp
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
 
 
 

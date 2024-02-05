422a5d62a8a6dc3746c257ae3d2c71ae46e91e2b
Reyan A <iamreyan@proton.me>
2024-02-05 19:34:48 +0500

    day 10 till Q.6

--- day_10/loops.py
+++ day_10/loops.py
@@ -1,5 +1,8 @@
+import math
+
 number = 1
 h = '#'
+t = 8
 
 for number in range(11):
     print(number)
@@ -11,18 +14,21 @@ while number < 11:
 for y in range(6):
     print(h*y)
 
+print('0 x 0 = 0')
+print('1 x 1 = 1')
+print('2 x 2 = 4')
+print('3 x 3 = 9')
+print('4 x 4 = 16')
+print('5 x 5 = 25')
+print('6 x 6 = 36')
+print('7 x 7 = 49')
+print('8 x 8 = 64')
+print('8 x 9 = 81')
+print('10 x 10 = 100')
 
-
-
-
-
-
-
-
-
-
-
-
+lists = ['Python', 'Numpy', 'Pandas', 'Django', 'Flask']
+for keys in lists:
+    print(lists)
 
 
 
